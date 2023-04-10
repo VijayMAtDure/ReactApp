bd041590907011efaaa7dc034278b479c2953113
VijayMAtDure <vijay.mishra@duretechnologies.com>
2023-04-10 19:07:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my world!
+          Welcome to my world of react
         </p>
         <a
           className="App-link"
