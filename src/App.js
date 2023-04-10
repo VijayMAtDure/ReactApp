c3f385bf147f2fd4416240b871b121ad35ae33d2
VijayMAtDure <vijay.mishra@duretechnologies.com>
2023-04-10 19:06:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to branch specific
+          Welcome to branch specific of Azure
         </p>
         <a
           className="App-link"
